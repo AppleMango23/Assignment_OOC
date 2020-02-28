8d942a1cf03a9c415e8535c31f4f209679bbce8c
Noah Yek <ninjayek@gmail.com>
2020-02-28 13:26:38 +0800

    Added some output and decision

---
+++ Assignment_OOC/Source.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int decision = 0;
+
+	cout << "=======Welcome to Marina Bay=======" << endl;
+	while(decision != 4)
+	{
+		cout << "What you want to do: " << endl;
+		cout << "1. Record a new booking" << endl;
+		cout << "2. Delete a record" << endl;
+		cout << "3. Display all record and available marina space" << endl;
+		cout << "4. Exit the program" << endl << endl;
+
+		cout << "Decision: ";
+		cin >> decision;
+		cout << endl;
+	}
+
+	cout << "\n-----------------" << endl;
+	cout << "Application exited" << endl << endl;
+}
